ac95f1300202848f4abe0da34836f07c09c17d76
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-08-02 08:55:45 -0700

    Updated Code

--- src/Routing/routing.ts
+++ src/Routing/routing.ts
@@ -4,102 +4,102 @@ import { ObjectId } from 'mongodb';
 
 const userRouting = express.Router();
 
-userRouting.post('/createUser', async (request: express.Request, response: express.Response) => {
+// userRouting.post('/createUser', async (request: express.Request, response: express.Response) => {
 
-    try {
-        let database = getDatabase();
+//     try {
+//         let database = getDatabase();
 
-        let collection = database.collection('users');
+//         let collection = database.collection('users');
 
-        let body = request.body;
+//         let body = request.body;
 
-        const data = await collection.insertOne(body)
+//         const data = await collection.insertOne(body)
 
-        response.status(200).json({
-            "response": data
-        })
-
-
-    } catch (error) {
-        console.log(error)
-    }
-
-});
-
-userRouting.get('/getUsers', async (request: express.Request, response: express.Response) => {
-    try {
-        let database = getDatabase()
-        let collection = database.collection('users');
-        const get = await collection.find({}).toArray();
-
-        response.status(200).json({
-            'response': get
-        })
-
-    } catch (error) {
-
-    }
-})
-
-userRouting.get('/getsingleuser/:id', async (request: express.Request, response: express.Response) => {
-    try {
-        let userId = request.params.id;
-        let database = getDatabase()
-        let collection = database.collection('users');
-        const get = await collection.find({ "_id": new ObjectId(userId) }).toArray();
-
-        response.status(200).json({
-            'response': get
-        })
-
-    } catch (error) {
-        console.log(error)
-
-    }
-})
+//         response.status(200).json({
+//             "response": data
+//         })
 
 
-userRouting.put('/updateuser', async (request: express.Request, response: express.Response) => {
-    try {
-        let body = request.body;
-        let database = getDatabase()
-        let collection = database.collection('users');
-        let userinfo = {
-            "name": body.name,
+//     } catch (error) {
+//         console.log(error)
+//     }
 
-            "email": body.email
-        }
-        const update = await collection.updateOne({ "_id": new ObjectId(body.userId) }, { '$set': userinfo });
+// });
 
-        response.status(200).json({
-            'response': update
-        })
+// userRouting.get('/getUsers', async (request: express.Request, response: express.Response) => {
+//     try {
+//         let database = getDatabase()
+//         let collection = database.collection('users');
+//         const get = await collection.find({}).toArray();
 
-    } catch (error) {
-        console.log(error)
+//         response.status(200).json({
+//             'response': get
+//         })
 
-    }
-})
+//     } catch (error) {
+
+//     }
+// })
+
+// userRouting.get('/getsingleuser/:id', async (request: express.Request, response: express.Response) => {
+//     try {
+//         let userId = request.params.id;
+//         let database = getDatabase()
+//         let collection = database.collection('users');
+//         const get = await collection.find({ "_id": new ObjectId(userId) }).toArray();
+
+//         response.status(200).json({
+//             'response': get
+//         })
+
+//     } catch (error) {
+//         console.log(error)
+
+//     }
+// })
+
+
+// userRouting.put('/updateuser', async (request: express.Request, response: express.Response) => {
+//     try {
+//         let body = request.body;
+//         let database = getDatabase()
+//         let collection = database.collection('users');
+//         let userinfo = {
+//             "name": body.name,
+
+//             "email": body.email
+//         }
+//         const update = await collection.updateOne({ "_id": new ObjectId(body.userId) }, { '$set': userinfo });
+
+//         response.status(200).json({
+//             'response': update
+//         })
+
+//     } catch (error) {
+//         console.log(error)
+
+//     }
+// })
 
 
 
-userRouting.delete('/deleteuser/:id', async (request: express.Request, response: express.Response) => {
-    try {
-        let userId = request.params.id;
-        let database = getDatabase()
-        let collection = database.collection('users');
+// userRouting.delete('/deleteuser/:id', async (request: express.Request, response: express.Response) => {
+//     try {
+//         let userId = request.params.id;
+//         let database = getDatabase()
+//         let collection = database.collection('users');
 
-        const deleted = await collection.deleteOne({ "_id": new ObjectId(userId) });
+//         const deleted = await collection.deleteOne({ "_id": new ObjectId(userId) });
 
-        response.status(200).json({
-            'response': deleted
-        })
+//         response.status(200).json({
+//             'response': deleted
+//         })
 
-    } catch (error) {
-        console.log(error)
+//     } catch (error) {
+//         console.log(error)
 
-    }
-})
+//     }
+// })
 
 
 export default userRouting;
--- src/controller/user_controller.ts
+++ src/controller/user_controller.ts
@@ -1,6 +1,7 @@
 import express from "express"
-import { Db } from "mongodb"
+import { Db, ObjectId } from "mongodb"
 import { getDatabase } from "../config/database"
+import { User_Model } from "../model/user_model";
 
 export class User_controller {
 
@@ -11,6 +12,39 @@ export class User_controller {
 
         let userCollection = db.collection("users");
 
+        let user_Data: User_Model = request.body;
         
+        const validator = {
+            email: user_Data.email
+        };
+
+       let checking =  await userCollection.find(validator).toArray();
+        
+        if (checking.length != 0) {
+            response.status(403).send({
+                "status": "Failure",
+                "response": "Email Already Exist"
+            });
+        }
+        else {
+
+            let responseData = await userCollection.insertOne(user_Data);
+            
+            let User_Id = responseData.insertedId;
+
+            let User_FetchedData = await userCollection.find({ "_id": new ObjectId(User_Id) }).toArray();
+
+            let Object_Id = User_FetchedData[0];
+
+            response.status(200).send({
+                "status": "OK",
+                "response": "User Successfully SignUp",
+                "User Uid" : Object_Id
+            });
+        }
+
+
+
+
     }
 }
